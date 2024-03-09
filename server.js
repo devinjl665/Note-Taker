138cfb593ac0af6e6d5f25fd44898ca4110845a4
elroy <devinjl665@gmail.com>
2024-03-08 20:24:47 -0600

    Set listening port in server.js and added empty route files

--- server.js
+++ server.js
@@ -32,3 +32,9 @@ app.get('/notes', (req, res) =>
 app.get('/*', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
     );
+
+
+
+app.listen(PORT, () => 
+console.log(`App is listening at http://localhost:${PORT}`)
+);
